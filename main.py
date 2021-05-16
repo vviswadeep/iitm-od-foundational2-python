392d0c98a1b2de52701438c1418567ae02edef98
VISWADEEPV <>
2021-05-16 10:55:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+'''
+Week 2 Activity Questions
+'''
+
+#1
+# a=1
+# print(type(a))
+# a=1.0
+# print(type(a))
+# a='1'
+# print(type(a))
+
+#
---
+++ week1.py
@@ -0,0 +1,180 @@
+"""
+hey
+this is just a comment"""
+# Single Line Comment
+
+#First Question
+# Data Types we studied about
+#print("Int, Float, String, Boolean")
+#print("")
+
+#2 
+#print("String") #email
+#print("Float") #annual Income
+#print("Int") #no of days in a month
+#print("")
+
+#3
+#bin digit similar to
+"""
+bool
+switch
+coin toss
+tennis match outcome
+nodding head
+"""
+
+#4
+#a=7**35
+#print(str(a)[-1])
+#print("")
+
+#5
+#a=987654321%123456789
+#print(a)
+#print("")
+
+#6
+2 #print(5 - 3) 
+8 #print(5 - - 3)
+2 #print(5 - - - 3)
+8 #print(5 - - - - 3)
+2 #print(5 - - - - - 3)
+#print("")
+
+#7
+#print(2 ** (2 - 2) - 2 / 2)
+#print("")
+
+#8
+#True #print(0==0<1<2<3>2>1>0==0)
+#print("")
+
+#9
+#some_string="hello"
+#n=len(some_string)
+#print(len(n*some_string))
+#n**2
+
+#10
+#True
+#a="Hello"
+#b="World"
+#print(len(a)+len(b)==len(a+b))
+#print("")
+
+#11
+#correct
+#it gets the input and then displays back to the user
+#print(input())
+#print("")
+
+#12
+#1 then 2 and then 3
+#print(input()+input()+input())
+
+#13
+#print(12345678987654321**0.5)
+#print("")
+
+#14
+#print(len(str(7**100)))
+#print("")
+
+#15
+#a=input()
+#b=a[0] == a[-1]
+#print(b)
+#print("")
+"""
+ALL PALINDROME WORDS
+mam
+malayalam
+
+OTHER WORDS
+mangalam
+bomb
+skies
+spectacles
+spaces
+engage
+encourage
+differed
+disturbed
+differentiated
+saves
+tangent
+discord
+"""
+
+#16
+#a = int(input())
+#print(int(str(a)+'0'))
+#print("")
+
+#17
+#a=int(input("enter the length:"))
+#b=int(input('enter the breadth:'))
+#c=a*b
+#print(c)
+#print("")
+
+#18
+#it will give the volume of a box
+
+#19
+#a=int(input())
+#print(type(a))
+#print("")
+"""
+When the input is an integer it would be able to accept the input
+Since 1.23 is not an integer, input function by default considers that as a string data type
+So we get a value error with invalid literal
+"""
+
+#20
+#a=float(input())
+#b=int(a)
+#print(b)
+#print("")
+
+#21
+#a=float(input())
+#b=int(a)
+#c= b<a<b+1
+#print(b+int(c))
+#print("")
+
+#22
+#a = int(input())
+#b = 90<=a<=100
+#print(b)
+#print("")
+
+#23
+#8 pairs
+# (1,2),(2,3)....(8,9)
+
+#24
+#a=int(input())
+#b=int(str(a)[0])
+#c=int(str(a)[1])
+#print(b+c)
+#print("")
+"""
+Convert the int to a string and then consider the face value of each digit and then add it accordingly
+"""
+
+#25
+#a=int(input())
+#b=a**0.5
+#c =  b-int(b)
+#print(c==0)
+
+
+""""
+a=1/3 33.33
+b=1/2 50.00
+c=1/4 25.00
+b-a=1/6 16.67
+"""
